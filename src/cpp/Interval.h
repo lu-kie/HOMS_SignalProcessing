387dc15436fe37c456bbb7df0b97292bb14b5c4b
lu-kie <77334517+lu-kie@users.noreply.github.com>
2021-03-08 17:14:12 +0100

    Update Interval.h

--- src/cpp/Interval.h
+++ src/cpp/Interval.h
@@ -12,14 +12,14 @@ private:
     int l; //Left bound of discrete interval
     int r; //Right bound of discrete interval
     double eps; //Approximation error for interval
-	double beta; // Smoothing penalty of interval
+    double beta; // Smoothing penalty of interval
     vec data; // Interval data
 public:
     // Getter:
     int getL();
     int getR();
     double getEps();
-	double getBeta();
+    double getBeta();
     vec getData();
     // Setter:
     void setEps(double e);
@@ -28,12 +28,12 @@ public:
     void setData(vec data);
     // Destructor
     ~Interval();
-	// Constructor for finding an optimal partition
+    // Constructor for finding an optimal partition
     Interval(int left, int right, double data_new, const int k, double alpha);
     // Constructor for the signal reconstruction from a partition
     Interval(int left, int right, vec y, const int k, double alpha);
     // Give interval / data length
-    int giveLength() ;
+    int giveLength();
     // Add data point to the bottom of the interval for updating the corresp. approximation error
     void addBottomDataPoint(const int k, mat &C, mat &S, double data_new);
     // Update associated data i.e. sparse Givens rotate it (for reconstruction process)
